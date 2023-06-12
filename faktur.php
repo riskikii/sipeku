0d68d82a771b48fb9fec749bfaa7dbb9d0bdc819
Akmal Rafi Nursyabani <akmalrafi.yourdev@gmail.com>
2023-06-12 23:44:19 +0700

    Penyewaan dan Laporan Pendapatan Sub Rinci - WIP

--- faktur.php
+++ faktur.php
@@ -7,19 +7,18 @@
         <div class="card-body">
             <div class="table-responsive">
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                  <thead align="center">
-                    <tr>
-                        <th>No</th>                        
-                        <th>Fasilitas</th>
-                        <th>Tgl Pemakaian</th>
-                        <th>Vol</th>
-                        <th>Harga</th>
-                        <th>Jumlah</th>
-                        <th>Status</th>
-                        <th>Action</th>
-                    </tr>
-                  </thead>
-                  <tfoot  align="center">
+                    <thead align="center">
+                        <tr>
+                            <th>No</th>
+                            <th>Fasilitas</th>
+                            <th>Tgl Pemakaian</th>
+                            <th>Vol</th>
+                            <th>Jumlah</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <!-- <tfoot  align="center">
                     <tr>
                         <th>No</th>
                         <th>Fasilitas</th>
@@ -30,61 +29,63 @@
                         <th>Status</th>
                         <th>Action</th>
                     </tr>
-                  </tfoot>
-                  <tbody>
-                    <?php
-                            require_once 'utils/CustomDateFormatter.php';
-                            require_once 'controller/PenyewaanController.php';
+                  </tfoot> -->
+                    <tbody>
+                        <?php
+                        require_once 'utils/CustomDateFormatter.php';
+                        require_once 'controller/PenyewaanController.php';
 
-                            $formatter = new CustomDateFormatter();
-                            $penyewaan = new PenyewaanController();
+                        $formatter = new CustomDateFormatter();
+                        $penyewaan = new PenyewaanController();
 
-                            $listData = $penyewaan->getListofPenyewaan();
+                        $listData = $penyewaan->getListofPenyewaan();
 
-                            if ($penyewaan != null) {
-                                $no = 1;
-                                foreach ($listData as $data) {
-                            ?>
-                    <tr>
-                      <td><center><?php echo $no++ ?></center></td>
-                      <td><?php echo $data['referensi'] ?></td>
-                      <td><?php echo $formatter->indonesian($data['tanggal']) ?></td>
-                      <td><?php echo $data['volume'] ?></td>
-                      <td>Rp.<?php echo number_format($data['nilai'], 0, '.', ","); ?></td>
-                      <td>Rp.<?php echo number_format($data['total'], 0, '.', ","); ?></td>
-                      <td> 
-                        <?php if ( $data['status'] == "lunas"){
-                            echo "<p style='color:white;background-color: green;text-align:center; border-radius:10px; padding:10px 10px 10px 10px'>Lunas</p>";
-                        } 
-                        else if ( $data['status'] == "belum bayar"){
-                            echo "<p style='color:white;background-color: #FFC300;text-align:center; border-radius:10px;padding:10px 10px 10px 10px; color:white'>Belum Bayar</p>";
-                        }
+                        if ($listData > 0) {
+                            $no = 1;
+                            foreach ($listData as $data) {
                         ?>
-                    </td>
-
-
-                                    <td><center>
-                                        <a href="print_faktur.php?id_penyewaan=<?php echo $data['id_transaksi']; ?>" target="_blank" class="btn btn-primary btn-icon-split" >
-                                            <span class="icon text-white-50">
-                                                <i class="fas fa-print"></i>
-                                            </span>
-                                        </a>
-                                        <a href="?url=edit_data_penyewaan&id_penyewaan=<?php echo $data['id_transaksi']; ?>" class="btn btn-warning btn-icon-split">
-                                            <span class="icon text-white-50">
-                                                <i class="fas fa-edit"></i>
-                                            </span>
-                                        </a>
-                                        <a href="#" onclick="confirmDelete(<?php echo $data['id_transaksi']; ?>)" class="btn btn-danger btn-icon-split">
-                                            <span class="icon text-white-50">
-                                                <i class="fas fa-trash"></i>
-                                            </span>
-                                        </a></center>
+                                <tr>
+                                    <td>
+                                        <center><?php echo $no++ ?></center>
                                     </td>
-                    </tr><?php
-                            }
-                        } else {
+                                    <td><?php echo $data['referensi'] ?></td>
+                                    <td><?php echo $formatter->indonesian($data['tanggal']) ?></td>
+                                    <td><?php echo $data['volume'] ?></td>
+                                    <td>Rp.<?php echo number_format($data['nilai'], 0, '.', ","); ?></td>
+                                    <td>
+                                        <?php if ($data['status'] == "lunas") {
+                                            echo "<p style='color:white;background-color: green;text-align:center; border-radius:10px; padding:10px 10px 10px 10px'>Lunas</p>";
+                                        } else if ($data['status'] == "belum bayar") {
+                                            echo "<p style='color:white;background-color: #FFC300;text-align:center; border-radius:10px;padding:10px 10px 10px 10px; color:white'>Belum Bayar</p>";
+                                        }
+                                        ?>
+                                    </td>
 
-                            ?>
+
+                                    <td>
+                                        <center>
+                                            <a href="print_faktur.php?id_penyewaan=<?php echo $data['id_transaksi']; ?>" target="_blank" class="btn btn-primary btn-icon-split">
+                                                <span class="icon text-white-50">
+                                                    <i class="fas fa-print"></i>
+                                                </span>
+                                            </a>
+                                            <a href="?url=edit_data_penyewaan&id_penyewaan=<?php echo $data['id_transaksi']; ?>" class="btn btn-warning btn-icon-split">
+                                                <span class="icon text-white-50">
+                                                    <i class="fas fa-edit"></i>
+                                                </span>
+                                            </a>
+                                            <a href="#" onclick="confirmDelete(<?php echo $data['id_transaksi']; ?>)" class="btn btn-danger btn-icon-split">
+                                                <span class="icon text-white-50">
+                                                    <i class="fas fa-trash"></i>
+                                                </span>
+                                            </a>
+                                        </center>
+                                    </td>
+                                </tr><?php
+                                    }
+                                } else {
+
+                                        ?>
                             <tr>
                                 <td colspan="8" class="text-center h5 py-3">Data tidak ditemukan.</td>
                             </tr>
--- lap_peritem.php
+++ lap_peritem.php
@@ -1,14 +1,80 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <!-- Header -->
     <h1 class="h3 mb-4 text-gray-800">Laporan Per Item</h1>
 
+    <table class="table table-bordered">
+        <tr>
+            <td>Uraian</td>
+            <td>Januari</td>
+            <td>Februari</td>
+            <td>Maret</td>
+            <td>April</td>
+            <td>Mei</td>
+            <td>Juni</td>
+            <td>Juli</td>
+            <td>Agustus</td>
+            <td>September</td>
+            <td>Oktober</td>
+            <td>November</td>
+            <td>Desember</td>
+            <td>Jumlah</td>
+        </tr>
+
+        <?php
+        require_once 'controller/PenyewaanController.php';
+        $class = new PenyewaanController();
+
+        $laporanPerItem = $class->laporanPenyewaanPerItem();
+        print_r($laporanPerItem);
+        $jumlah = 0;
+        $pendapatanJanuari = 0;
+        $pendapatanFebruari = 0;
+        $pendapatanMaret = 0;
+        $pendapatanApril = 0;
+        $pendapatanMei = 0;
+        $pendapatanJuni = 0;
+        $pendapatanJuli = 0;
+        $pendapatanAgustus = 0;
+        $pendapatanSeptember = 0;
+        $pendapatanOktober = 0;
+        $pendapatanNovember = 0;
+        $pendapatanDesember = 0;
+
+        if ($laporanPerItem != null) {
+            foreach ($laporanPerItem as $data) {
+        ?>
+
+                <tr>
+                    <td><?php echo ($data['nama_sub_kategori_retribusi'] == null) ? $data['nama_kategori_retribusi'] : $data['nama_sub_kategori_retribusi'] ?></td>
+                    <td><?php ($data['MONTH(tanggal)'] == 1) ? $pendapatanJanuari = $data["SUM('nilai')"] : $pendapatanJanuari; echo $pendapatanJanuari ?></td>
+                    <td>33.032.000</td>
+                    <td>32.424.000</td>
+                    <td>32.208.000</td>
+                    <td>45.258.000</td>
+                    <td>5.000.000</td>
+                    <td>30.528.000</td>
+                    <td>5.000.000</td>
+                    <td>5.000.000</td>
+                    <td>1.250.000</td>
+                    <td>2.350.000</td>
+                    <td>195.000.000</td>
+                </tr>
+        <?php
+            }
+        }
+        ?>
+    </table>
+
 </body>
+
 </html>
